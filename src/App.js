3ba9f647d830bb5138cd2d8a81e31b5e47787570
Angela Malone <a.malone1193@gmail.com>
2018-04-16 18:23:46 -0400

    route setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Calculator from "./Calculator.js";
+import Calculator from "./Calculator";
+import Hacker from "./Hacker";
+import { Route, Link, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Calculator />
+      <div>
+        <nav className="Navigation">
+          <h1>Project 3</h1>
+          <Link to="/">Home</Link>
+          <Link to="/terminal-hacker">Terminal Hacker</Link>
+          <Link to="/happiness-calculator">Happiness Calculator</Link>
+        </nav>
+        <main>
+          <Switch>
+            <Route path="/terminal-hacker" render={() => <Hacker />} />
+            <Route path="/happiness-calculator" render={() => <Calculator />} />
+          </Switch>
+        </main>
       </div>
     );
   }
--- src/Hacker.js
+++ src/Hacker.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 
 class Hacker extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
       p1: "",
       p2: "",
@@ -29,40 +29,45 @@ class Hacker extends Component {
     });
   }
 
-  render() {
-    return;
-    <form onSubmit={this.handleSubmit}>
-      Password 1:
-      <input type="text" name="p1" onChange={this.handleChange} />
-      Password 2:
-      <input type="text" name="p2" onChange={this.handleChange} />
-      Password 3:
-      <input type="text" name="p3" onChange={this.handleChange} />
-      Password 4:
-      <input type="text" name="p4" onChange={this.handleChange} />
-      Password 5:
-      <input type="text" name="p5" onChange={this.handleChange} />
-      Password 6:
-      <input type="text" name="p6" onChange={this.handleChange} />
-      Password 7:
-      <input type="text" name="p7" onChange={this.handleChange} />
-      Password 8:
-      <input type="text" name="p8" onChange={this.handleChange} />
-      Password 9:
-      <input type="text" name="p9" onChange={this.handleChange} />
-      Password 10:
-      <input type="text" name="p10" onChange={this.handleChange} />
-      Password 11:
-      <input type="text" name="p11" onChange={this.handleChange} />
-      Password 12:
-      <input type="text" name="p12" onChange={this.handleChange} />
-      <input type="submit" value="Submit" />
-    </form>;
+  handleSubmit(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
 
-    <h3>Possible Passwords:</h3>;
-    {
-      choices;
-    }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          Password 1:
+          <input type="text" name="p1" onChange={this.handleChange} />
+          Password 2:
+          <input type="text" name="p2" onChange={this.handleChange} />
+          Password 3:
+          <input type="text" name="p3" onChange={this.handleChange} />
+          Password 4:
+          <input type="text" name="p4" onChange={this.handleChange} />
+          Password 5:
+          <input type="text" name="p5" onChange={this.handleChange} />
+          Password 6:
+          <input type="text" name="p6" onChange={this.handleChange} />
+          Password 7:
+          <input type="text" name="p7" onChange={this.handleChange} />
+          Password 8:
+          <input type="text" name="p8" onChange={this.handleChange} />
+          Password 9:
+          <input type="text" name="p9" onChange={this.handleChange} />
+          Password 10:
+          <input type="text" name="p10" onChange={this.handleChange} />
+          Password 11:
+          <input type="text" name="p11" onChange={this.handleChange} />
+          Password 12:
+          <input type="text" name="p12" onChange={this.handleChange} />
+          <input type="submit" value="Submit" />
+        </form>
+        <h3>Possible Passwords:</h3>
+      </div>
+    );
   }
 }
 
